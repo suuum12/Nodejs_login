48a6b4a318424dc2128d52353ebcacc77e2777bd
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-02-10 01:08:45 +0900

    라우터 수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const {User} = require('../models');
 const router = express.Router();
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 
-router.get('/', (req,res,next)=>{
-    res.render('index');
+router.get('/',(req,res,next)=>{
+    if(req.isAuthenticated()){
+        res.render('index',{
+            text:req.user.nick,
+            isLoggedin:req.isAuthenticated(),
+        });
+    }else{
+        res.render('index',{
+            text:'로그인 필요',
+            isLoggedin:req.isAuthenticated(),
+        });
+    }
+});
+
+router.post('/change', async (req, res, next) => {
+    try { 
+    await User.update({ nick: req.body.changenick }, {
+      where: { id: req.user.id },
+       });
+       res.redirect('/');
+   }catch(e){
+       console.error(e);
+       next(e);
+   }
 });
 
 module.exports=router;
